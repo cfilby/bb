8b4dfb0d00ef944feff32797b74797da31130ba5
Cooper Filby <cfilby2@uabmc.edu>
2019-10-26 19:42:24 -0500

    Initial commit

---
+++ lib/bb.ex
@@ -0,0 +1,18 @@
+defmodule Bb do
+  @moduledoc """
+  Documentation for Bb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
